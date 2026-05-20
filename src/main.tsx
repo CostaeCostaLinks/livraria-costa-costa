6d1f1959c92c06ef77aaa1ee48a7c56a1ea91978
Cleverson <cleverson128@hotmail.com>
2026-05-20 18:31:56 -0300

    correção main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// Adicione a importação:
 import { HelmetProvider } from 'react-helmet-async'
+
+// Esta é a linha que estava faltando para carregar o Tailwind e os estilos:
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* Envolva o App com o HelmetProvider */}
     <HelmetProvider>
       <App />
     </HelmetProvider>
